a481dec709094aab1d16c46140b2ecbbfe575b4d
root <936645698@qq.com>
2026-03-24 04:25:36 +0000

    nothing

--- download_hf.py
+++ download_hf.py
@@ -1,7 +1,7 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(
-    repo_id = "JohnnyZeppelin/CVIQ",
+    repo_id = "JohnnyZeppelin/backup",
     repo_type = "dataset",
-    local_dir="./data/CVIQ"
+    local_dir="./"
 )
